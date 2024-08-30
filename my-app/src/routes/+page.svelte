860f321827ef838d39d2c45ac48e1397b617c02d
hugo-chabert <hugo.chabert@laplateforme.io>
2024-08-30 15:56:22 +0200

    [ADD] login-form

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Button from "@/components/ui/button/button.svelte";
+
+
+</script>
+
+<a href="/login"><Button variant="outline">Se connecter</Button></a>
