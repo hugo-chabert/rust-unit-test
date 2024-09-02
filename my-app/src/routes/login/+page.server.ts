a2beac943900931fdf37a78c0d9dac3e83983581
hugo-chabert <hugo.chabert@laplateforme.io>
2024-09-02 09:55:25 +0200

    [UPDATE] databse now working

--- my-app/src/routes/login/+page.server.ts
+++ my-app/src/routes/login/+page.server.ts
@@ -3,23 +3,28 @@ import { fail } from "@sveltejs/kit";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { formSchema } from "./schema";
- 
+import Users from "$models/Users.js";
+
 export const load: PageServerLoad = async () => {
- return {
-  form: await superValidate(zod(formSchema)),
- };
-};
- 
-export const actions: Actions = {
- default: async (event) => {
-  const form = await superValidate(event, zod(formSchema));
-  if (!form.valid) {
-   return fail(400, {
-    form,
-   });
-  }
+
+  const users = await Users.find()
+  console.log(users);
+
   return {
-   form,
+    form: await superValidate(zod(formSchema)),
   };
- },
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    const form = await superValidate(event, zod(formSchema));
+    if (!form.valid) {
+      return fail(400, {
+        form,
+      });
+    }
+    return {
+      form,
+    };
+  },
 };
--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -14,6 +14,7 @@ const config = {
 		adapter: adapter(),
 		alias: {
 			"@/*": "./src/lib/*",
+			$models: "src/models",
 		}
 	}
 };
